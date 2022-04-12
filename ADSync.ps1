a1e33ce2c6aff78392f1ff69a0fd27b7aefc497c
Donny Maasland <donny@unauthorizedaccess.nl>
2022-04-12 14:10:59 +0200

    add example ad sync script

--- ADSync.ps1
+++ ADSync.ps1
@@ -1,4 +1,16 @@
 Import-Module -Name '.\AzureADScanner'
+Import-Module Microsoft.Graph.Identity.DirectoryManagement
 
-$Computers = Get-Content -Path 'computers.json' | ConvertFrom-Json
-$Computers | Invoke-AzureADSync -Token ""
+# Connect to MS Graph API using certificate, see: https://docs.microsoft.com/graph/powershell/app-only?tabs=azure-portal for instructions
+$ClientId = "00000000-0000-0000-0000-000000000000"
+$TenantId = "00000000-0000-0000-0000-000000000000"
+$CertificateFile = "path\to\certificate.p12"
+
+Connect-MgGraph -ClientID $ClientId -TenantId $TenantId -Certificate $CertificateFile # Cert can also be loaded from certstore on windows
+
+# Get devices from AAD, see https://docs.microsoft.com/powershell/module/microsoft.graph.identity.directorymanagement/get-mgdevice for options
+$AADDevices = Get-MgDevice
+
+# Synchronize devices
+$Token = "<base64 token>"
+$AADDevices | Invoke-AzureADSync -Token $Token
